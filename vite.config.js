3cd9e1d7163279c0e40cb93a40d046979ff3f8cf
Jose Uribe <joseuribeh@gmail.com>
2025-04-22 20:39:27 -0500

    fix: cambios en configuracion de vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/scout-groups-finder/',
-})
+  base: "/scout-groups-finder/",
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  resolve: {
+    alias: {
+      crypto: "crypto-js",
+    },
+  },
+});
