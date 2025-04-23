2ab72dddaa4b3f1a272fa0dc0dacf0443bc73a69
Jose Uribe <joseuribeh@gmail.com>
2025-04-22 20:57:35 -0500

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/scout-groups-finder/",
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
