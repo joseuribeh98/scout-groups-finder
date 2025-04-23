34b974be19073036701e6d130b1d9a83ac8b2121
Jose Uribe <joseuribeh@gmail.com>
2025-04-22 21:25:47 -0500

    fix: config vite file change

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/scout-groups-finder/",
   build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-  resolve: {
-    alias: {
-      crypto: "crypto-js",
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
     },
   },
 });
